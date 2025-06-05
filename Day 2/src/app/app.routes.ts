d3642fcad8caa4d850284aedae03391a85b4e9f2
dhruvilbavarva <dhruvilbavarva@gmail.com>
2025-06-05 17:23:36 +0530

    initial commit

---
+++ Day 2/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Menu1Component } from './menu1/menu1.component';
+
+export const routes: Routes = [
+    {
+        path:'menu1',
+        component:Menu1Component
+    }
+];
