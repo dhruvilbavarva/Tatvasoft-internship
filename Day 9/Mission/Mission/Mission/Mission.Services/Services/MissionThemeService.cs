3d57aeeadfad9dafbee190d414de2c8b92f9bd59
dhruvilbavarva <dhruvilbavarva@gmail.com>
2025-06-05 18:02:22 +0530

    initial commit

---
+++ Day 9/Mission/Mission/Mission/Mission.Services/Services/MissionThemeService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mission.Entities.Entities;
+using Mission.Entities.Models;
+using Mission.Repositories.IRepositories;
+using Mission.Services.IServices;
+
+namespace Mission.Services.Services
+{
+    public class MissionThemeService(IMissionThemeRepository missionThemeRepository) : IMissionThemeService
+    {
+        private readonly IMissionThemeRepository _missionThemeRepository = missionThemeRepository;
+        public Task<bool> AddMissionTheme(MissionThemeViewModel model)
+        {
+            MissionTheme missionTheme = new MissionTheme()
+            {
+                Id = model.Id,
+                Status = model.Status,
+                ThemeName = model.ThemeName,
+            };
+            return _missionThemeRepository.AddMissionTheme(missionTheme);//add mission
+        }
+
+        public Task<bool> DeleteMissionTheme(int missionThemeId)
+        {
+            return _missionThemeRepository.DeleteMissionTheme(missionThemeId);//delete mission
+        }
+
+        public Task<List<MissionThemeViewModel>> GetAllMissionTheme()
+        {
+            return _missionThemeRepository.GetAllMissionTheme();//get all mission
+        }
+
+        public Task<MissionThemeViewModel?> GetMissionThemeById(int missionThemeId)
+        {
+            return _missionThemeRepository.GetMissionThemeById(missionThemeId);//get mission
+        }
+
+        public Task<bool> UpdateMissionTheme(MissionThemeViewModel model)
+        {
+            MissionTheme missionTheme = new MissionTheme()
+            {
+                Id = model.Id,
+                Status = model.Status,
+                ThemeName = model.ThemeName,
+            };
+            return _missionThemeRepository.UpdateMissionTheme(missionTheme);//upadate mission
+        }
+    }
+}
